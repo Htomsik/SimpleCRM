7bb3811f9c464e17c767d167778248dbc2a9a899
Konstantin <83224669+Htomsik@users.noreply.github.com>
2022-08-30 16:49:54 +0300

    Добавление тестов для Шин сообщений

---
+++ ProjectMateTaskWpfTests/InfrastructureTests/MessageBusTests/LoggerMessageBusTests.cs
@@ -0,0 +1,11 @@
+﻿using ProjectMateTask.Infrastructure.MessageBuses;
+using ProjectMateTaskWpfTests.InfrastructureTests.MessageBusTests.BaseMessageBusTests;
+
+namespace ProjectMateTaskWpfTests.InfrastructureTests.MessageBusTests;
+
+[TestClass]
+public class LoggerMessageBusTests : BaseMessageBusAbstractTests<LoggerMessageBus,string>
+{
+    protected override string GenerateSomeMessage() => "SomeMessage";
+
+}
