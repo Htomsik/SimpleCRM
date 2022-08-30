cfc5b8de26df846da9132bae099ed9971d7c133b
Konstantin <83224669+Htomsik@users.noreply.github.com>
2022-08-30 12:41:40 +0300

    Добавление реализации для базового хранилища. BaseStore.cs

---
+++ ProjectMateTask/Stores/Base/BaseStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProjectMateTask.Stores.Base;
+
+/// <summary>
+///     Бзовая реализация хранилища
+/// </summary>
+/// <typeparam name="T">Любой класс</typeparam>
+public class BaseStore<T> : IStore<T> where T : class
+{
+    protected Lazy<T?> _currentValue;
+
+    public virtual T? CurrentValue
+    {
+        get => _currentValue?.Value;
+        set
+        {
+            _currentValue = new Lazy<T>(() => value);
+            OnCurrentValueChanged();
+        }
+    }
+
+    public event Action? CurrentValueChanged;
+
+    /// <summary>
+    ///     Метод, обновляющий увидомитель
+    /// </summary>
+    protected virtual void OnCurrentValueChanged()
+    {
+        CurrentValueChanged?.Invoke();
+    }
+}
